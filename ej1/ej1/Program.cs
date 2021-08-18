28d3ea4ab7525871275728540dc13835f5d7e1a1
JuanMoli111 <hombrebota@hotmail.com>
2021-08-18 18:42:15 -0300

    tp1 ej1 resuelto

---
+++ ej1/ej1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ej1
+{
+    class Ejercicio1
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hola ");
+            Console.Read();
+            Console.Write("Mundo");
+        }
+    }
+}
