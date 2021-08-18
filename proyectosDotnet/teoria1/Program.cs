45714ccb4186364699a379796bf14dabbcd6392a
JuanMoli111 <hombrebota@hotmail.com>
2021-08-18 18:37:16 -0300

    Hola mundo en DotNet

---
+++ proyectosDotnet/teoria1/Program.cs
@@ -0,0 +1,13 @@
+﻿//using System;
+
+namespace teoria1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hola Mundo! Un saludo a toda la comunidad de programadores!!!");
+
+        }
+    }
+}
