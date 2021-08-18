ef9acabb2205e4e2f4009a6442020a8025c0de2e
JuanMoli111 <hombrebota@hotmail.com>
2021-08-18 19:26:29 -0300

    tp1 ej2 resuelto

---
+++ proyectosDotnet/ej2/SecuenciasDeEscape.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ej2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Daaaaaaaaamn\n!");       //Siguiente linea
+            
+            Console.WriteLine("Daaaaaaaaamn\b!");       //Retroceso (El caracter retrocede pisando al anterior)
+            Console.WriteLine("Daaaaaaaaamn\t!");       //Tabulacion
+
+            Console.WriteLine("Daaaaaaaamn\r!");       //Retorno de carro 
+            Console.WriteLine("Daaaaaaaamn\v!");       //Tabulacion vertical
+  
+
+            Console.WriteLine("Daaaa\\aaaaamn!");       //La doble barra invertida imprime una barra invertida, evitando que se confunda con otras secuencias de escape
+            Console.WriteLine("Daaaaa\"aaaamn!");       //Permite imprimir el caracter de comilla evitando que se confunda con el cierre de la cadena de texto
+
+        }
+    }
+}
