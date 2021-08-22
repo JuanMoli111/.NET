fa4974cfeb9abe84e1881ed55a431a71956542c5
JuanMoli111 <hombrebota@hotmail.com>
2021-08-21 23:11:36 -0300

    tp1 ej12 resuelto

---
+++ proyectosDotnet/ej10/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ej10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            for(int i = 0; i <= 1000; i++)
+            {
+                if((i % 17 == 0) | (i % 29 == 0))
+                {
+                    Console.WriteLine(i);
+                }
+            
+            }
+        }
+    }
+}
---
+++ proyectosDotnet/ej12/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ej12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string st = Console.ReadLine();
+
+            int num = int.Parse(st);
+
+            
+            for(int i = 1; i <= num;i++)
+            {
+                
+                if(num % i == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+        }
+    }
+}
