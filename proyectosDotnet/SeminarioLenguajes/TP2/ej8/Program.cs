834a5ab975962ad7bbb410029632b8c49a0c9bd4
JuanMoli111 <hombrebota@hotmail.com>
2021-08-26 21:14:48 -0300

    tp2 ej8 resuelto

---
+++ proyectosDotnet/SeminarioLenguajes/TP2/ej8/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ej8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //inciso a
+
+            for(int i = 0; i < args.Length; i++)
+            {
+                Console.WriteLine("Hola " + args[i]);
+            }
+
+            Console.WriteLine();
+
+            //inciso b
+
+            foreach (string nombre in args)
+            {
+                Console.WriteLine("Hola " + nombre);
+            }
+        }
+    }
+}
