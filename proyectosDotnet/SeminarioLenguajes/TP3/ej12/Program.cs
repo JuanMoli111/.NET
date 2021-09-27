9eb61740e34e72f2da0a22a17f71020376e7fde0
JuanMoli111 <hombrebota@hotmail.com>
2021-09-27 20:17:12 -0300

    p3 e12 resuelto

--- proyectosDotnet/SeminarioLenguajes/TP3/ej12/Program.cs
+++ proyectosDotnet/SeminarioLenguajes/TP3/ej12/Program.cs
@@ -8,7 +8,7 @@ namespace ej12
     class Program
     {
 
-        static Hashtable cargarTabla()
+        static Hashtable cargarTablaCifrado()
         {
             Hashtable tabla = new Hashtable();
 
@@ -26,32 +26,61 @@ namespace ej12
             }
 
             //Agregar manualmente el caracter de espacio
-            tabla.Add((object)"sp",28);
+            tabla.Add(' ',28);
 
             return tabla;
         }
+
+        static Hashtable cargarTablaDescifrado()
+        {
+            Hashtable tabla = new Hashtable();
+
+            //Agregar caracteres hasta la N
+            for(int num = 1, Ascii = 65; Ascii <= 78; num++, Ascii++){
+                tabla.Add(num,(char)Ascii);
+            }
+
+            //Agregar la Ñ manualmente 
+            tabla.Add(15,'Ñ');     
+
+            //Agregar los caracteres desde la O hasta la Z
+            for(int num = 16, Ascii = 79; Ascii <= 90; num++, Ascii++){                
+                tabla.Add(num,(char)Ascii);
+            }
+
+            //Agregar manualmente el caracter de espacio
+            tabla.Add(28,' ');
+
+            return tabla;
+        }
+
 
         static void mostrarTabla(Hashtable tabla)
         {
 
             foreach (var keys in tabla.Keys)
             {
-                Console.WriteLine($"Para la clave {keys} el valor es {tabla[keys]}");
+                Console.WriteLine($"Para la clave {keys} el tipo es {tabla[keys].GetType()}");
             }
-
+            foreach (var value in tabla.Values)
+            {
+                Console.WriteLine($"Para el valor {value} el tipo es {value.GetType()}");
+            }
         }
 
 
+        
 
 
         static void Main(string[] args)
         {
             
-            Hashtable tabla = cargarTabla();
+            //Declarar e inicializar las hashtable
+            Hashtable tablaCifrado = cargarTablaCifrado();
+            Hashtable tablaDescifrado = cargarTablaDescifrado();
 
 
-            mostrarTabla(tabla);
-
+            //mostrarTabla(tablaCifrado);
 
 
             //LEER TEXTO
@@ -63,47 +92,78 @@ namespace ej12
             Console.WriteLine("Ingresar las claves separadas por coma");
             string[] ArregloClaves = Console.ReadLine().Split(",");
 
+            //Declarar arreglo de enteros con tantos elementos como caracteres del texto
+            //aqui se guardara el codigo cifrado 
+            int[] CodigoCifrado = new int[texto.Length];
 
+            //Declara un string vacio donde almacenaremos el mensaje cifrado resultante
+            string MensajeCifrado = "";
+
+
+            //Declarar e inicializar el queue
             Queue ColaClaves = new Queue();
 
+            //Cargar las claves en el queue
             for(int j = 0; j < ArregloClaves.Length; j++)
             {
                 ColaClaves.Enqueue(ArregloClaves[j]);
             }
             
 
-            //FOR CADA CHAR DEL TEXTO
-                //ENCOLAMOS EL PRIMER ELEM, CONSULTANDOLO CON PEEK()
-                //DESENCOLAMOS CLAVE
-                //CACLULAR CODIGO CIFRADO USANDO CLAVE
+            //CIFRADO
+            //Para cada caracter del texto
+            for(int i = 0; i < texto.Length; i++)
+            {
+   
+                //Encolar clave, ej para la cola 3,5,6 queda 3,5,6,3
+                ColaClaves.Enqueue(ColaClaves.Peek());
+
+                //Calcular la clave numerica cifrada para este caracter, 
+                //consiguiendo su valor por hashtable y sumandole el desplazamiento segun la clave desencolada               
+                int numCifrado = (int)tablaCifrado[texto[i]] + int.Parse((string)ColaClaves.Dequeue());    
+
+
+                //Guardar cada num cifrado para conservar el codigo cifrado generado
+                CodigoCifrado[i] = numCifrado;
+
+                //Concatenar el char correspondiente, con el num calculado en numCifrado, en el hashtable de descifrado
+                //SI EL RESULTADO ES MAYOR A 28 CALCULAR (RESULTADO MOD 28) ESTE RESTO ES EL CODIGO EFECTIVO
+                MensajeCifrado += numCifrado <= 28 ? tablaDescifrado[numCifrado] : tablaDescifrado[numCifrado % 28]; 
+
+            }
             
+        
+            Console.WriteLine($"Mensaje cifrado: {MensajeCifrado}");
 
-            //Declarar arreglo de enteros con tantos elementos como caracteres el texto, para cada clave correspondiente a cada char
-            int[] CodigoCifrado = new [texto.Length];
+            ColaClaves.Clear();
 
-            //Declarar un int como variable auxiliar para 
-            int clave;
+            for(int j = 0; j < ArregloClaves.Length; j++)
+            {
+                ColaClaves.Enqueue(ArregloClaves[j]);
+            }
 
+
+            string MensajeDescifrado = "";
+
+            //DESCIFRADO
+            //Para cada caracter del texto
             for(int i = 0; i < texto.Length; i++)
             {
    
                 //Encolar la primer clave, ej para la cola 3,5,6 queda 3,5,6,3
                 ColaClaves.Enqueue(ColaClaves.Peek());
 
+                //Calcular la clave descifrada, restando la clave al numero cifrado anteriormente                
+                int numDescifrado = CodigoCifrado[i] - int.Parse((string)ColaClaves.Dequeue());// - (int)tablaCifrado[texto[i]];    
 
-                int numCifrado = tabla[texto[i]] + ColaClaves.Dequeue();
-                
-
-                //guardar en el arreglo de enteros codigoCifrado, el codigo cifrado correspondiente a este char,
-                // para calcularlo conseguir el valor con el hashtable y sumarle la clave desencolandola
-                CodigoCifrado[i] = tabla[texto[i]] + ColaClaves.Dequeue();
-
-
-                
-                //SI RESULTADO ES MAYOR A 28 CALCULAR (RESULTADO MOD 28) ESTE RESTO ES EL CODIGO EFECTIVO
-                numCifrado <= 28 ? numCifrado : numCifrado Mod 28;
-
+                //Concatenar el caracter correspondiente al numero descifrado 
+                MensajeDescifrado += tablaDescifrado[numDescifrado]; 
+            
+            
             }
+            
+            Console.WriteLine($"Mensaje descifrado: {MensajeDescifrado}");
+
 
 
         }
